84bce8fa3c7dacfd8c146b90dcb48ac9f794347e
ABSanthosh <a.b.santhosh02@gmail.com>
2025-07-26 22:50:37 -0400

    feat: grid implementation

--- src/entrypoints/newtab/App.svelte
+++ src/entrypoints/newtab/App.svelte
@@ -2,10 +2,29 @@
 
 <script lang="ts">
   import settingStore from "../../lib/stores/settingStore";
+  import WidgetGrid from "../../lib/components/WidgetGrid.svelte";
+
+  let widgetGrid: WidgetGrid;
+
+  // Optional: Access grid information
+  function getGridInfo() {
+    if (widgetGrid) {
+      return widgetGrid.getGridInfo();
+    }
+    return null;
+  }
 
   settingStore.subscribe((value) => {
     document.body.style.backgroundImage = `url(${value.options.wallpaper})`;
   });
 </script>
 
-hello there $$render
+<WidgetGrid 
+  bind:this={widgetGrid}
+  minWidgetSize={120}
+  gridGap={10}
+  gridPadding={40}
+  showGrid={true}
+>
+  <!-- Widget components will go here -->
+</WidgetGrid>
